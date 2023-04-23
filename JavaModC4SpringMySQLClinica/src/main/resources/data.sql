c7a697ab27ac5110b0621c77b43206338b370b76
Aitor Iglesias <iglesiaspubillaitor@gmail.com>
2023-04-23 22:25:18 +0200

    SQL finished

--- JavaModC4SpringMySQLClinica/src/main/resources/data.sql
+++ JavaModC4SpringMySQLClinica/src/main/resources/data.sql
@@ -4,70 +4,47 @@ DROP TABLE IF EXISTS investigadores;
 DROP TABLE IF EXISTS facultad;
 
 
-CREATE TABLE IF NOT EXISTS facultad (
-    codigo INT AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS roles (
+    id INT AUTO_INCREMENT,
     nombre VARCHAR(100) DEFAULT NULL,
-    PRIMARY KEY(codigo)
+    PRIMARY KEY(id)
     );
     
-CREATE TABLE IF NOT EXISTS investigadores (
+CREATE TABLE IF NOT EXISTS usuarios (
     id INT AUTO_INCREMENT,
-    dni VARCHAR(8),
-    nom_apels VARCHAR(255) DEFAULT NULL,
-    facultad INT,
+    name VARCHAR(100) DEFAULT NULL,
+    email VARCHAR(255) DEFAULT NULL,
+    role INT,
     PRIMARY KEY(id),
-    CONSTRAINT facultad_investigadores_fk FOREIGN KEY (facultad) REFERENCES facultad (codigo)
+    CONSTRAINT role_fk FOREIGN KEY (role) REFERENCES roles (id)
     ON DELETE CASCADE ON UPDATE CASCADE
 	);
     
-CREATE TABLE IF NOT EXISTS equipos (
+CREATE TABLE IF NOT EXISTS citas (
     id INT AUTO_INCREMENT,
-    numserie CHAR(4),
-    nombre VARCHAR(100) DEFAULT NULL,
-    facultad INT,
+    fecha DATETIME DEFAULT NULL,
+    usuario INT,
     PRIMARY KEY(id),
-    CONSTRAINT facultad_equipos_fk FOREIGN KEY (facultad) REFERENCES facultad (codigo)
+    CONSTRAINT usuario_fk FOREIGN KEY (usuario) REFERENCES usuarios (id)
     ON DELETE CASCADE ON UPDATE CASCADE
     );
 
-CREATE TABLE IF NOT EXISTS reserva (
-    id INT AUTO_INCREMENT,
-    comienzo DATETIME DEFAULT NULL,
-    fin DATETIME DEFAULT NULL,
-    investigador INT,
-    equipo INT,
-    PRIMARY KEY(id),
-    KEY (investigador, equipo),
-    CONSTRAINT investigador_fk FOREIGN KEY (investigador) REFERENCES investigadores(id)
-    ON DELETE CASCADE ON UPDATE CASCADE,
-    CONSTRAINT equipo_fk FOREIGN KEY (equipo) REFERENCES equipos(id)
-    ON DELETE CASCADE ON UPDATE CASCADE
-    );
+/* INSERT INTO roles */
+INSERT INTO roles(nombre) VALUES ('USUARIO');
+INSERT INTO roles(nombre) VALUES ('ADMINISTRADOR');
 
-/* INSERT INTO facultad */
-INSERT INTO facultad(nombre) VALUES ('FACULTAD 1');
-INSERT INTO facultad(nombre) VALUES ('FACULTAD 2');
-INSERT INTO facultad(nombre) VALUES ('FACULTAD 3');
-INSERT INTO facultad(nombre) VALUES ('FACULTAD 4');
-INSERT INTO facultad(nombre) VALUES ('FACULTAD 5');
+/* INSERT INTO usuarios */
+INSERT INTO usuarios (name, email, role) VALUES ('Aitor', 'aaa@aaa', 2);
+INSERT INTO usuarios (name, email, role) VALUES ('Blai', 'bbb@bbb', 1);
+INSERT INTO usuarios (name, email, role) VALUES ('Carlos', 'ccc@ccc', 1);
+INSERT INTO usuarios (name, email, role) VALUES ('Daniel', 'ddd@ddd', 1);
+INSERT INTO usuarios (name, email, role) VALUES ('Edgar', 'eee@eee', 1);
+INSERT INTO usuarios (name, email, role) VALUES ('Fran', 'fff@fff', 1);
 
-/* INSERT INTO investigadores */
-INSERT INTO investigadores (dni, nom_apels, facultad) VALUES ('00000001', 'Investigador 1', 1);
-INSERT INTO investigadores (dni, nom_apels, facultad) VALUES ('00000002', 'Investigador 2', 2);
-INSERT INTO investigadores (dni, nom_apels, facultad) VALUES ('00000003', 'Investigador 3', 3);
-INSERT INTO investigadores (dni, nom_apels, facultad) VALUES ('00000004', 'Investigador 4', 4);
-INSERT INTO investigadores (dni, nom_apels, facultad) VALUES ('00000005', 'Investigador 5', 5);
 
-/* INSERT INTO equios */
-INSERT INTO equipos (numserie, nombre, facultad) VALUES ('01', 'equipo 1', 1);
-INSERT INTO equipos (numserie, nombre, facultad) VALUES ('02', 'equipo 2', 2);
-INSERT INTO equipos (numserie, nombre, facultad) VALUES ('03', 'equipo 3', 3);
-INSERT INTO equipos (numserie, nombre, facultad) VALUES ('04', 'equipo 4', 4);
-INSERT INTO equipos (numserie, nombre, facultad) VALUES ('05', 'equipo 5', 5);
-
-/* INSERT INTO reserva */
-INSERT INTO reserva (comienzo, fin, investigador, equipo) VALUES ('2019-03-27 14:15:37.000000', '2025-12-17 14:15:37', 1, 1);
-INSERT INTO reserva (comienzo, fin, investigador, equipo) VALUES ('2020-03-28 14:15:37.000000', '2025-12-17 14:15:37', 2, 2);
-INSERT INTO reserva (comienzo, fin, investigador, equipo) VALUES ('2021-03-29 14:15:37.000000', '2025-12-17 14:15:37', 3, 3);
-INSERT INTO reserva (comienzo, fin, investigador, equipo) VALUES ('2022-03-30 14:15:37.000000', '2025-12-17 14:15:37', 4, 4);
-INSERT INTO reserva (comienzo, fin, investigador, equipo) VALUES ('2023-03-31 14:15:37.000000', '2025-12-17 14:15:37', 5, 5);
+/* INSERT INTO citas */
+INSERT INTO citas (fecha, usuario) VALUES ('2019-03-27 14:15:37.000000', 2);
+INSERT INTO citas (fecha, usuario) VALUES ('2020-03-28 14:15:37.000000', 3);
+INSERT INTO citas (fecha, usuario) VALUES ('2021-03-29 14:15:37.000000', 4);
+INSERT INTO citas (fecha, usuario) VALUES ('2022-03-30 14:15:37.000000', 5);
+INSERT INTO citas (fecha, usuario) VALUES ('2023-03-31 14:15:37.000000', 6);
